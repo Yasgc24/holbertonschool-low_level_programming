6395ea06a76d0039f6b612ebfd82d63d885392ef
Yasgc24 <4527@holbertonschool.com>
2022-02-10 11:38:38 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
+
 #include <stdio.h>
-/*
-*low level programming
+/**
+* main - Starting
+* Return: 0
 */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
