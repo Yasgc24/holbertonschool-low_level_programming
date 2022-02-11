733807b45ed23a78b0c7077c5e22979d0ef5d270
Yasgc24 <4527@holbertonschool.com>
2022-02-11 14:09:54 -0800

    Alphaqe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - Alphabet
+*Return: 0
+*/
+int main(void)
+{
+char alp;
+for (alp = 'a'; alp <= 'z'; alph != 'q'; alp != 'e'; alp++)
+putchar(alp);
+putchar('\n');
+return (0);
+}
