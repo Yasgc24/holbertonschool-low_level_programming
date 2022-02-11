ffba751408682e6059858d6e3154b1eb3e4fe515
Yasgc24 <4527@holbertonschool.com>
2022-02-11 13:49:23 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /*
-*main - Alphabet uppercase
+*main - Alphabet
 *Retur: 0
 */
 int main(void)
 {
 char alp;
+char alpH;
 for (alp = 'a'; alp <= 'z'; alp++)
 putchar(alp);
-for (alp = 'A'; alp <= 'Z'; alp++)
-putchar(alp);	
+for (alpH = 'A'; alpH <= 'Z'; alpH++)
+putchar(alpH);
 putchar('\n');
 return (0);
 }
