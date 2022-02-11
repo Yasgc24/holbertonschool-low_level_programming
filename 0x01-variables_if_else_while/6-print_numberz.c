6f8cc0cc73beaca806a9ac9adff5427593c8c32f
Yasgc24 <4527@holbertonschool.com>
2022-02-11 15:52:46 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 /**
 *main - Numberz
 *Return: 0
 */
+
 int main(void)
 {
 int n;
-for (n = '0'; n < '10'; n++)
+for (n = '0'; n <= '9'; n++)
+{
 putchar(n);
+}
 putchar('\n');
 return (0);
 }
