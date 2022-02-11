8a0a58da14d2ae29a47aae9a2c8b7d74ff1e274c
Yasgc24 <4527@holbertonschool.com>
2022-02-11 15:01:46 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
-*main - Numbers of base 10
-*Return: 0
+* main - Numbers of base 10
+* Return: 0
 */
 int main(void)
 {
 int n;
-if (n >= 0)
-putchar(n);
-putchar('\n');
-if (n < 10)
+for (n >= 0; n < 10)
 putchar(n);
 putchar('\n');
 return (0);
