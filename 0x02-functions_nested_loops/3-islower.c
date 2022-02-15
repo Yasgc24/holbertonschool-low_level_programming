4a3fd6669f239ef97104529927ef73cd2c344063
Yasgc24 <4527@holbertonschool.com>
2022-02-15 14:02:54 -0800

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - checks for lowercase
+* @c: character
+* Return: 0
+*/
+
+int _islower(int c)
+{
+for (c >= 'a'; c <= 'z'; c++)
+{
+return (1);
+}
+return (0);
+}
