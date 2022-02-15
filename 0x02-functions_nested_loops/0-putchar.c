e95a5d0a68163c2a4a8806114e4211f9600870fd
Yasgc24 <4527@holbertonschool.com>
2022-02-15 06:54:48 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,14 @@
 */
 int main(void)
 {
-char p;
-p = _putchar;
-putchar(p);
+putchar("_");
+putchar("p");
+putchar("u");
+putchar("t");
+putchar("c");
+putchar("h");
+putchar("a");
+putchar("r");
 putchar("\n");
 return (0);
 }
