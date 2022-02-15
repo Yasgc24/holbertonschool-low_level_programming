08ca36a2efeb06d039fc47a864b506ff31e1075d
Yasgc24 <4527@holbertonschool.com>
2022-02-15 07:23:29 -0800

    main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - Alphabet
+* Return: 0
+*/
+
+void print_alphabet(void)
+{
+char alp;
+for (alp = 'a'; alp <= 'z'; alp++)
+putchar(alp);
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+int purchar(char c);
