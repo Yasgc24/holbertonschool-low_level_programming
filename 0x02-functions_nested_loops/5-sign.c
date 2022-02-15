0450ba027b408b9d615f36636a898c53336d3c72
Yasgc24 <4527@holbertonschool.com>
2022-02-15 15:31:37 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - Prints the sign of a number
+* @n: character
+* Return: 0
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
