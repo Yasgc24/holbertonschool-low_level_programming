bbdb9a7903278b561457c959134abf67c0a40596
Yasgc24 <4527@holbertonschool.com>
2022-02-15 20:16:34 -0800

    Natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_to_98 - Prints all natural numbers
+* @n: character
+* Return: 0
+*/
+
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+_putchar(n);
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
