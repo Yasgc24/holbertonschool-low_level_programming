4a802d42bc3b9a133747cbcf636ca1df21ee55e6
Yasgc24 <4527@holbertonschool.com>
2022-02-17 08:35:23 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_numbers - Prints numbers from 0 to 9
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+_putchar(n);
+_putchar('\n');
+}
