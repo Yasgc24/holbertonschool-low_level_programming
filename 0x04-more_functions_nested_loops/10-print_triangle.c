8f1c09b04bc3a50b0f1de0330951aa92718b25cd
Yasgc24 <4527@holbertonschool.com>
2022-02-17 19:16:11 -0800

    Triangle #

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* print_triangle - Prints a triangle
+* @size: character
+* Return: 0
+*/
+
+void print_triangle(int size)
+{
+int a;
+int b;
+
+if (size > 0)
+{
+for (a = 1; a < size; a++)
+{
+for (b = 1; b < size; b++)
+{
+if (b <= size - a)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
