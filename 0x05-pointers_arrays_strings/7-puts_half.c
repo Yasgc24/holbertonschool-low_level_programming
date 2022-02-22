7d12c08e3bf5eaa1f9c424f1b7c67c47cc5162eb
Yasgc24 <4527@holbertonschool.com>
2022-02-21 17:16:57 -0800

    Prints half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: char
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+int n = 0;
+int a = 0;
+int b;
+
+{
+while (str[n++])
+a++;
+
+if ((a & 2) == 0)
+b = a / 2;
+
+else
+b = ((a + 1) / 2);
+
+for (n = b; n < a; n++)
+_putchar(str[n]);
+}
+_putchar('\n');
+}
