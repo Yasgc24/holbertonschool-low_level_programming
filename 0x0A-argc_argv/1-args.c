d226280b31a4917403dea83d1ff8090718fc6846
Yasgc24 <4527@holbertonschool.com>
2022-03-04 09:40:01 -0800

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: size
+ * @argv: string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%i\n", argc - 1);
+
+return (0);
+}
