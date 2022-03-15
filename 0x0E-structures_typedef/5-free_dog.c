036aaff3bb36f87ec4f12eafd306d55ff27504f7
Yasgc24 <4527@holbertonschool.com>
2022-03-14 19:17:58 -0700

    frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer
+ * Return: free the memory
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
