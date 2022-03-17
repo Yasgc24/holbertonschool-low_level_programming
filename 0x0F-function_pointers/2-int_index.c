aaab8e482043f1b307995ac8aa1bce6703ec30f9
Yasgc24 <4527@holbertonschool.com>
2022-03-17 08:36:20 -0700

    executes a function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: size
+ * @cmp: pointer
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0 || array == NULL || cmp == NULL)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
