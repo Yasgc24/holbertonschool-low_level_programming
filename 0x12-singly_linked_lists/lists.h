063c0e9e1c20d53c10b8785ed1d04796153e74ab
Yasgc24 <4537@holbertonschool.com>
2022-03-29 09:44:47 -0700

    Add main h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct lists
+{
+char *str;
+unsigned int len;
+struct lists* next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
