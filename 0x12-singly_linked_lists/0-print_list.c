ad9a7c8e12a99e1c9b65fd942d656e77c1ed14e0
Yasgc24 <4537@holbertonschool.com>
2022-03-29 10:04:37 -0700

    Correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,10 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-while (h != NULL)
+size_t count;
+for (count = 0; h != NULL; count++)
 {
-count++;
 if ((*h).str == NULL)
 printf("[0] (nil)\n");
 else
