22ab9f9f0090064708f848fdef4c53ad20cae1ff
Yasgc24 <4537@holbertonschool.com>
2022-03-29 19:17:34 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+list_t *vatemp;
+
+while (head != NULL)
+{
+vatemp = head;
+head = head->next;
+free(vatemp);
+}
+}
