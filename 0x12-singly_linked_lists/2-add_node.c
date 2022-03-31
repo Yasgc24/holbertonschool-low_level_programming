b436b6779f4e840e68bd624bf7a699a9fa32904d
Yasgc24 <4537@holbertonschool.com>
2022-03-31 09:21:45 -0700

    Correction finally

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
+ * add_node - adds a new node at the beginning of a list
  * @head: head
  * @str: string
- * Reutnr: the address of the new element
+ * Return: the address of the new element
  */
 
 list_t *add_node(list_t **head, const char *str)
