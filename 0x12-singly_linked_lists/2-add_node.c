34bc161145e18764c9e9afd2eac382ad2664d6b4
Yasgc24 <4537@holbertonschool.com>
2022-03-31 09:15:05 -0700

    Correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,13 +9,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node = new_node = (list_t *)malloc(sizeof(list_t));
+list_t *new_node;
+new_node = (list_t *)malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 
 new_node->str = strdup(str);
-if (new_node->str == NULL || new_node->next == NULL)
+if (new_node->str == NULL);
+{
+free(new_node);
 return (NULL);
+}
+
 new_node->len = strlen(new_node->str);
 new_node->next = (*head);
 (*head) = new_node;
