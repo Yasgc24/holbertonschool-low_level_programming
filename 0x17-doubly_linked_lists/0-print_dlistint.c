c51cd877e7ce990123b031343c0857d90f07f1a3
Yasgc24 <4537@holbertonschool.com>
2022-05-12 10:57:16 -0700

    Add correction task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,15 @@
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nds = 0;
 
 	while (h != NULL)
 	{
 		nds++;
-		printf("%d/n", h->nds);
-		h = h->next;
+		printf("%d\n", (*h).n);
+		h = (*h).next;
 	}
 	return (nds);
 }
