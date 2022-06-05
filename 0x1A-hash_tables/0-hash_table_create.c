455e88f567920823b8311a863fb212987386a0ca
Yasgc24 <4537@holbertonschool.com>
2022-06-05 09:33:12 -0500

    Add directory

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table or NULL
+ * if something went wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+    if (table == NULL)
+    {
+        free(table);
+        return NULL;
+    }
+
+    table->size = size;
+    table->array = malloc(sizeof(hash_node_t *) * size);
+    if (table->array == NULL)
+    {
+        free(table);
+        return NULL;
+    }
+    return (table);
+}
