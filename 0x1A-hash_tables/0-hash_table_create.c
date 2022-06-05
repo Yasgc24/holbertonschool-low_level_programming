894a3dedf49d1cfa16b1a1fed8dd2c25700092d8
Yasgc24 <4537@holbertonschool.com>
2022-06-05 09:36:40 -0500

    Add betty correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-    if (table == NULL)
-    {
-        free(table);
-        return NULL;
-    }
+	if (table == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-    table->size = size;
-    table->array = malloc(sizeof(hash_node_t *) * size);
-    if (table->array == NULL)
-    {
-        free(table);
-        return NULL;
-    }
-    return (table);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	return (table);
 }
