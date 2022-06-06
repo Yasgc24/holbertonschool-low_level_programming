37b3a72c0392a89388e8214e6e6ee6482c12494c
Yasgc24 <4537@holbertonschool.com>
2022-06-06 10:17:32 -0500

    Add task 2 and 4

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives you the index of a key
+ * @key: is the key
+ * @size: the size of the array
+ * Return: the index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    if (key == NULL)
+    {
+        return (0);
+    }
+    return (hash_djb2(key) % size);
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: is the hash table
+ * @key: is the key
+ * Return: the value associated with
+ * the element, or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    hash_node_t *node;
+    unsigned int index;
+
+    if (ht == NULL)
+        return (NULL);
+
+    {
+        index = key_index((const unsigned char *)key, ht->size);
+
+        if (ht->array[index])
+        {
+            node = ht->array[index];
+            if (node)
+            {
+                if (strcmp(node->key, (char *)key) == 0)
+                    return (node->value);
+                node = node->next;
+            }
+        }
+    }
+    return (NULL);
+}
