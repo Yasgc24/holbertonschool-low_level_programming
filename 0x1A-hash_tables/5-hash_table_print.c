be9463537c8e98165ac9f63d41e58aa92341819f
Yasgc24 <4537@holbertonschool.com>
2022-06-06 14:58:33 -0500

    Add task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: is the hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	char *flag;
+
+	if (!ht)
+		return;
+	printf("{");
+	flag = "";
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			printf("%s'%s': '%s'", flag, node->key, node->value);
+			flag = ", ";
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
