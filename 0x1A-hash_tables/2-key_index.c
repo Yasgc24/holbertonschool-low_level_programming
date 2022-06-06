ca9bd7a5edc4cda9cd19c9bf9e16d252b4826b2d
Yasgc24 <4537@holbertonschool.com>
2022-06-06 10:27:23 -0500

    key_index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,9 +9,9 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    if (key == NULL)
-    {
-        return (0);
-    }
-    return (hash_djb2(key) % size);
+	if (key == NULL)
+	{
+		return (0);
+	}
+	return (hash_djb2(key) % size);
 }
