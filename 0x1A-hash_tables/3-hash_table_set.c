761a1c114046090bc7fb9b17f074ab18e488e8e5
Yasgc24 <4537@holbertonschool.com>
2022-06-06 11:41:56 -0500

    Add task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,48 +10,44 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *node, *current;
-    unsigned long int index;
+	hash_node_t *node, *current;
+	unsigned long int index;
 
-    if (!ht || !key || *key == '\0' || !value)
-    {
-        return (0);
-    }
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    if (ht->array[index])
-    {
-        current = ht->array[index];
-        while (current != NULL)
-        {
-            if (strcmp(current->key, (char *)key) == 0)
-            {
-                free(current->value);
-                return (1);
-            }
-            current = current->next;
-        }
-        
-    }
-    node = (hash_node_t *)malloc(sizeof(hash_node_t));
-    if (!node)
-    {
-        return (0);
-    }
-    node->key = strdup(key);
-    if (!node->key)
-    {
-        free(node);
-        return (0);
-    }
-    node->value = strdup(value);
-    if (!node->value)
-    {
-        free(node->key);
-        free(node);
-        return (0);
-    }
-    node->next = ht->array[index];
-    ht->array[index] = node;
-    return (1);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index])
+	{
+		current = ht->array[index];
+		while (current != NULL)
+		{
+			if (strcmp(current->key, (char *)key) == 0)
+			{
+				free(current->value);
+				return (1);
+			}
+			current = current->next;
+		}
+	}
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
